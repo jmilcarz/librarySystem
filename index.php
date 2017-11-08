b9d8df965d902bac9565f3f8c5dc688fda00751a
kmilcarz <j.milcarz@yahoo.com>
2017-11-08 15:57:58 +0100

    first page styling

--- index.php
+++ index.php
@@ -14,6 +14,156 @@
 </head>
 <body>
      <?php require_once("./modules/nav.mod.php"); ?>
-     
+     <div id="main-container">
+          <section id="soon">
+               <h1 class="section-header">Niedługo minie termin oddania</h1>
+               <div class="grid">
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/18274810_450468415293985_5791790352588547985_n.jpg?oh=5d0595f948104bbc5f8adcd2015ffcfb&oe=5AA990E3" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        2 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>524632</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jakub Milcarz</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/c0.128.533.533/1016533_558894057539757_370222866_n.jpg?oh=00e46abb67aa1472da8ef9043df94a9b&oe=5A6558F3" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        8 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>426124</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Włodek Markowicz</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/19511494_780968788752461_1757222649295254114_n.jpg?oh=27e5cc0f1fdfce684ab5e54187faf7ce&oe=5AA16C5D" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        11 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>463221</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jan Pietruszka</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/19511494_780968788752461_1757222649295254114_n.jpg?oh=27e5cc0f1fdfce684ab5e54187faf7ce&oe=5AA16C5D" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        11 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>463221</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jan Pietruszka</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+               </div>
+               <a href="" class="btn btn-section">pokaż wszystko</a>
+          </section>
+
+          <section id="comed">
+               <h1 class="section-header">Minął Termin oddania</h1>
+               <div class="grid">
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/18274810_450468415293985_5791790352588547985_n.jpg?oh=5d0595f948104bbc5f8adcd2015ffcfb&oe=5AA990E3" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        2 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>524632</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jakub Milcarz</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/c0.128.533.533/1016533_558894057539757_370222866_n.jpg?oh=00e46abb67aa1472da8ef9043df94a9b&oe=5A6558F3" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        8 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>426124</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Włodek Markowicz</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/19511494_780968788752461_1757222649295254114_n.jpg?oh=27e5cc0f1fdfce684ab5e54187faf7ce&oe=5AA16C5D" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        11 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>463221</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jan Pietruszka</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+                    <div class="person">
+                         <div class="reminder">
+                              <img src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/19511494_780968788752461_1757222649295254114_n.jpg?oh=27e5cc0f1fdfce684ab5e54187faf7ce&oe=5AA16C5D" alt="">
+                              <div class="howmanydays">
+                                   <a href="">
+                                        11 dni
+                                        <span>"80 dni do okoła świata" - Juliusz Vernee</span>
+                                        <span>463221</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="info">
+                              <h3 class="name">Jan Pietruszka</h3>
+                              <a href="" class="btn">wyślij przypomnienie mailem</a>
+                              <a href="" class="btn">Przedłuż</a>
+                         </div>
+                    </div>
+               </div>
+               <a href="" class="btn btn-section">pokaż wszystko</a>
+          </section>
+     </div>
 </body>
 </html>
--- modules/nav.mod.php
+++ modules/nav.mod.php
@@ -4,9 +4,16 @@
                <h1><?php echo $system_name; ?></h1>
           </a>
           <ul class="links">
-               <li><a href=""><i class="fa fa-plus"></i> dodaj nową książkę</a></li>
-               <li><a href=""><i class="fa fa-database"></i> baza książek</a></li>
-               <li><a href=""><i class="fa fa-cogs"></i> ustawienia</a></li>
+               <li><a href=""><i class="fa fa-plus"></i> wypożycz książkę</a></li>
+               <li><a href=""><i class="fa fa-undo"></i> zwróć książkę</a></li>
+               <li id="toggledropdown">więcej <i class="fa fa-caret-down"></i>
+                    <ul class="dropdown">
+                         <li><a href=""><i class="fa fa-plus"></i> dodaj książkę</a></li>
+                         <li><a href=""><i class="fa fa-database"></i> baza książek</a></li>
+                         <li><a href=""><i class="fa fa-graduation-cap"></i> baza uczni</a></li>
+                         <li><a href=""><i class="fa fa-cogs"></i> ustawienia</a></li>
+                    </ul>
+               </li>
                <li>
                     <a href=""><form action="" method="post">
                          <button type="submit" name="logoutbtn"><i class="fa fa-sign-out"></i> Wyloguj się</button>
